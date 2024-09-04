65bb5915eb50b8f95d7e8682ed6f50e7c8417182
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2024-09-04 21:41:54 +0000

    Add function get_username which asks user to enter the username

--- run.py
+++ run.py
@@ -15,3 +15,21 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('language_quiz')
 
+def get_username():
+    """
+    Prompts the user to create a username.
+    Ensures the username is at least 4 characters long.
+    Returns:
+        username (str): The validated username entered by the user.
+    """
+    print("Welcome to the Language Quiz!\n")
+    while True:
+        print("Before the game starts - you need to create a username. Username should be more than 4 letters.\n")
+        username = input("Enter a username: \n")
+
+        if validate_username(username):
+            break
+        
+    return username
+
+
