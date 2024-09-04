e38d5f0171f750a65d5df25002e8450b862828e3
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2024-09-04 21:41:14 +0000

    Add comments for google shhets section

--- run.py
+++ run.py
@@ -2,12 +2,14 @@ import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
+# Define the scope for Google Sheets and Drive API
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
+# Authenticate using the service account credentials
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
